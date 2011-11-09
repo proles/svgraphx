ef45dce3fd2be73be8a481d52499600da75a67dd
Andres Bastidas <bastidas.andres@gmail.com>
2011-11-08 21:42:50 -0700

    add method to draw_grid based on output from make_grid, add method to draw discrete sine waves

--- __init__.py
+++ __init__.py
@@ -3,6 +3,10 @@ __init__.py
 
 Created by Andres Bastidas on 2011-11-07.
 """
+import math
+import random
+from nodebox.graphics import Context
+_ctx = Context
 
 def bounds(paths=[]):
   """ Returns (x,y), (width, height) bounds for a group of paths
@@ -29,7 +33,7 @@ def zero_translate(paths=[]):
   (x,y), (w,h) = bounds(paths)
   return (-x,-y)
 
-def make_grid(paths=[],xoffset=0,yoffset=0,xcount=1,ycount=1,xpad=0,ypad=0):
+def make_grid(paths=[], xoffset=0, yoffset=0, xcount=1, ycount=1, xpad=0, ypad=0):
   """ creates a grid using the provided path
   """
   g_paths = []
@@ -47,3 +51,32 @@ def make_grid(paths=[],xoffset=0,yoffset=0,xcount=1,ycount=1,xpad=0,ypad=0):
       g_paths.append(row)
 
   return g_paths
+
+def draw_grid(grid=[]):
+	""" draws provided grid to screen, expects output of make_grid
+	"""
+	for row in grid:
+		for item in row:
+			_ctx.push()
+			_ctx.translate(item['x'],item['y'])
+			_ctx.drawpath(item['path'].copy())
+			_ctx.pop()
+			
+	return
+
+def draw_sine(amplitude=1, period=180, step=1, xzero=0, yzero=0,palette=[],variation=False):
+	""" creates discrete sine wave with given amplitude,
+	half-period(degrees), and increment steps
+	"""
+	for angle in range(step,period,step):
+		x = angle + xzero
+		h = amplitude * math.sin(math.radians(angle))
+		if len(palette):
+			_ctx.fill(random.choice(palette))
+		if variation:
+			h = h * random.choice(range(80,100,5))/100
+		y = yzero - h
+		_ctx.rect(x, y, step, h)
+		print h
+	
+	return
